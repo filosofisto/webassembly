63f30dd170b5715c53fe227dd794bab0c018d235
Eduardo Ribeiro da Silva <filosofisto@gmail.com>
2022-12-04 10:43:01 +0100

    first commit

---
+++ Prime/comp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Compiling Project"
+
+emcc calculate_primes.c -o html_template.html
---
+++ Prime/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "Starting Server"
+
+python3 -m http.server 8080
+
